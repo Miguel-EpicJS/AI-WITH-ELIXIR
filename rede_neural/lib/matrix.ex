42192d07b06a9545f555445f7ba405cd5fd3a13c
CodigoDeSenior <migueljnm840@gmail.com>
2021-01-15 09:09:31 -0300

    feat: add and mult function for Matrix

--- rede_neural/lib/matrix.ex
+++ rede_neural/lib/matrix.ex
@@ -3,4 +3,13 @@ defmodule Main.Matrix do
     matrix = Matrex.new(rows, cols, fn () -> :rand.uniform(10) end)
     matrix
   end
+
+  def add(m1, m2) do
+    matrix3 = Matrex.new(m1[:rows], m1[:cols], fn (i, j) -> m1[i][j] + m2[i][j] end)
+    matrix3
+  end
+  def mult(m1, m2) do
+    matrix3 = Matrex.new(m1[:rows], m1[:cols], fn (i, j) -> m1[i][j] * m2[i][j] end)
+    matrix3
+  end
 end
