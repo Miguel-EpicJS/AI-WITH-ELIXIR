bac372e2e87f71e3fdfea4b7d616baabcce62268
CodigoDeSenior <migueljnm840@gmail.com>
2021-01-15 08:22:31 -0300

    Start files for REDE_NEURAL

---
+++ rede_neural/lib/main.ex
@@ -0,0 +1,18 @@
+defmodule Main do
+  @moduledoc """
+  Documentation for `RedeNeural`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RedeNeural.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ rede_neural/test/rede_neural_test.exs
@@ -0,0 +1,8 @@
+defmodule RedeNeuralTest do
+  use ExUnit.Case
+  doctest RedeNeural
+
+  test "greets the world" do
+    assert RedeNeural.hello() == :world
+  end
+end
